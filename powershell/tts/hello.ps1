92e65f30121d9d1370607d9c02b34ac029fb9a2d
Ben Aharoni <105626068+bendqh1@users.noreply.github.com>
2026-08-06 20:24:56 +0000

    Update hello.ps1

--- powershell/tts/hello.ps1
+++ powershell/tts/hello.ps1
@@ -3,7 +3,4 @@ Add-Type -AssemblyName System.Speech
 $synth = New-Object System.Speech.Synthesis.SpeechSynthesizer
 $synth.SelectVoice("Microsoft David Desktop")
 
-while ($true) {
-    Start-Sleep -Seconds (Get-Random -Minimum 120 -Maximum 241)
-    $synth.Speak("Hello")
-}
+$synth.Speak("Hello")
